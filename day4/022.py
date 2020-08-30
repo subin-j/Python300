7e703fe752fe16291145279245b2e95047bddc91
Domavem <61091132+Domavem@users.noreply.github.com>
2020-08-30 22:18:17 +0900

    day4

---
+++ day4/022.py
@@ -0,0 +1,2 @@
+liscense_plate = "24가 2210"
+print(liscense_plate[-4:])
---
+++ day4/025.py
@@ -0,0 +1,6 @@
+phone_number = "010-1111-2222"
+
+phone_number1 = phone_number.replace("-", " ")
+print(phone_number1)
+
+# 잘모르겠음
